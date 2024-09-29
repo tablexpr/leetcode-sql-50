4e507192c72940f505a7bcdc19c041b6a5ed709f
Tyler White <50381805+IndexSeek@users.noreply.github.com>
2024-09-29 11:11:31 -0400

    feat(exercise): continue 197 solution

--- exercises/197_rising_temperature.py
+++ exercises/197_rising_temperature.py
@@ -1,10 +1,11 @@
 # %% [markdown]
 # https://leetcode.com/problems/rising-temperature/description/?envType=study-plan-v2&envId=top-sql-50
 # %%
+from datetime import timedelta
+
 import pandas as pd
 import pyarrow as pa
 import pyarrow.compute as pc
-from datetime import timedelta
 
 # %% [markdown]
 # Setup the weather pandas DataFrame.
@@ -24,19 +25,22 @@ weather = pd.DataFrame(data, columns=["id", "recordDate", "temperature"]).astype
 table = pa.Table.from_pandas(weather)
 
 # %%
-previous_day = pc.add(table["recordDate"], pa.scalar(timedelta(days=-1)))
-lag_table = table.set_column(1, "previousDayDate", previous_day)
-
-# TODO: We don't need id in the lag table, we can drop it.
+lag_table = pa.table(
+    {
+        "recordDate": pc.add(table["recordDate"], pa.scalar(timedelta(days=1))),
+        "temperature": table["temperature"],
+    }
+)
 
 # %%
 joined = table.join(
     lag_table,
     keys="recordDate",
-    right_keys="previousDayDate",
     join_type="inner",
-    right_suffix="lag",
+    right_suffix="Lag",
 )
 
 # %%
-# TODO: Finish this problem.
+joined.filter(pc.greater(joined["temperature"], joined["temperatureLag"])).select(
+    ["id"]
+)
