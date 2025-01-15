996e9b3594b23a3c77944b8a9bda2e2423a5f3b1
Tyler White <50381805+IndexSeek@users.noreply.github.com>
2025-01-15 21:55:44 +0000

    test: deduplicate pytest params for problem 1070

--- tests/test_problem_params.py
+++ tests/test_problem_params.py
@@ -802,6 +802,42 @@ PARAMS_PROBLEM_1068 = [
     )
 ]
 
+
+PARAMS_PROBLEM_1070 = [
+    pytest.param(
+        {
+            "sale_id": [1, 2, 7],
+            "product_id": [100, 100, 200],
+            "year": [2008, 2009, 2011],
+            "quantity": [10, 12, 15],
+            "price": [5000, 5000, 9000],
+        },
+        {
+            "product_id": [100, 200],
+            "first_year": [2008, 2011],
+            "quantity": [10, 15],
+            "price": [5000, 9000],
+        },
+        id="basic_case_with_distinct_products",
+    ),
+    pytest.param(
+        {
+            "sale_id": [1, 2, 3, 4, 5],
+            "product_id": [100, 100, 200, 200, 200],
+            "year": [2008, 2009, 2011, 2012, 2013],
+            "quantity": [10, 20, 15, 5, 10],
+            "price": [5000, 5000, 9000, 9000, 9000],
+        },
+        {
+            "product_id": [100, 200],
+            "first_year": [2008, 2011],
+            "quantity": [10, 15],
+            "price": [5000, 9000],
+        },
+        id="duplicates_with_minimum_year_aggregation",
+    ),
+]
+
 PARAMS_PROBLEM_1204 = [
     pytest.param(
         {
--- tests/test_pyarrow.py
+++ tests/test_pyarrow.py
@@ -252,40 +252,7 @@ def test_problem_1068(input_data_1, input_data_2, expected_data):
 
 @pytest.mark.parametrize(
     "input_data, expected_data",
-    [
-        pytest.param(
-            {
-                "sale_id": [1, 2, 7],
-                "product_id": [100, 100, 200],
-                "year": [2008, 2009, 2011],
-                "quantity": [10, 12, 15],
-                "price": [5000, 5000, 9000],
-            },
-            {
-                "product_id": [100, 200],
-                "first_year": [2008, 2011],
-                "quantity": [10, 15],
-                "price": [5000, 9000],
-            },
-            id="basic_case_with_distinct_products",
-        ),
-        pytest.param(
-            {
-                "sale_id": [1, 2, 3, 4, 5],
-                "product_id": [100, 100, 200, 200, 200],
-                "year": [2008, 2009, 2011, 2012, 2013],
-                "quantity": [10, 20, 15, 5, 10],
-                "price": [5000, 5000, 9000, 9000, 9000],
-            },
-            {
-                "product_id": [100, 200],
-                "first_year": [2008, 2011],
-                "quantity": [10, 15],
-                "price": [5000, 9000],
-            },
-            id="duplicates_with_minimum_year_aggregation",
-        ),
-    ],
+    PARAMS_PROBLEM_1070,
 )
 def test_problem_1070(input_data, expected_data):
     input_table = pa.Table.from_pydict(input_data)
